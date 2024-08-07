aa8b9b07912db29bc9824f518926deedc2462f02
IamWafula <kni8@mail.com>
2024-08-06 23:11:10 -0700

    Added Name Generators

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import io
 
+import requests
+
 app = Flask(__name__)
 
 def humanize_data(data):
@@ -81,8 +83,81 @@ def generate_csv_normal_minmax():
     except Exception as e:
         return str(e), 400
 
+def getRandomNames(num_of_names, first_names, surnames):
+
+  surnames_sample = surnames.sample(n=num_of_names, replace=True).reset_index(drop=True)
+  first_names_sample = first_names.sample(n=num_of_names, replace=True).reset_index(drop=True)
+  result = pd.concat([first_names_sample, surnames_sample], axis=1)
+  result.columns = ['first_name', 'sex', 'surname']
+  result = result[['first_name', 'surname', 'sex']]
+  
+  return result
+
+
+def getRandomFirstName(num_of_names, gender, first_names):
+  first_names_cpy = first_names[first_names['sex'] == gender] if gender else first_names['name']
+  first_names_sample = first_names_cpy.sample(n=num_of_names, replace=True).reset_index(drop=True)
+
+  return first_names_sample
+
+def getRandomSecondName(num_of_names, surnames):
+  surnames_sample = surnames.sample(n=num_of_names, replace=True).reset_index(drop=True)
+  surnames_sample = surnames_sample.str.title()
+  return surnames_sample
+
+@app.route('/generate_csv_names', methods=["GET"])
+def generate_csv_names():
+    try:
+        gender = request.args.get('gender')
+        num_samples = int(request.args.get("num_samples"))
+        first_name = request.args.get("first_name")
+        surname = request.args.get("surname")
+
+        first_names = pd.read_csv('https://raw.githubusercontent.com/hadley/data-baby-names/master/baby-names.csv')
+        first_names = first_names.drop(['year', 'percent'], axis=1)
+        first_names['sex'] = first_names['sex'].replace({'boy': 'male', 'girl': 'female'})
+        first_names.drop_duplicates(inplace=True)
+
+        surnames = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/most-common-name/surnames.csv')
+        surnames = surnames['name'].str.title()
+        surnames.drop_duplicates(inplace=True)
+        
+        result = pd.DataFrame([])
+
+        # there has to be a better way to do this
+        if first_name == "True" and surname == "True":
+            result = getRandomNames(num_samples, first_names, surnames)
+
+            if gender:
+                result = result[result['sex']==gender]
+        
+        elif first_name == "True":
+            result = getRandomFirstName(num_samples, gender, first_names) if gender else  getRandomFirstName(num_samples, None, first_names)
+
+        else:
+            result = getRandomSecondName(num_samples, surnames)
+
+        # Convert DataFrame to CSV
+        output = io.StringIO()
+        result.to_csv(output, index=False)
+        output.seek(0)
+
+        # Create the response
+        response = make_response(output.getvalue())
+        response.headers['Content-Disposition'] = 'attachment; filename=generated_data_normal_minmax.csv'
+        response.headers['Content-Type'] = 'text/csv'
+
+        return response
+    except Exception as e:
+        return str(e), 400
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
+
 # TESTING:  curl -o generated_data_normal_std.csv "http://127.0.0.1:5000/generate_csv_normal_std?mean=10&std=5&num_samples=1000"
 # TESTING:  curl -o generated_data_normal_minmax.csv "http://127.0.0.1:5000/generate_csv_normal_minmax?mean=10&min=5&max=15&num_samples=1000"
+
+# change out params for gender, samples, first_name, surname
+# TESTING: curl -o generated_data_names.csv "http://127.0.0.1:5000/generate_csv_names?gender=male&num_samples=20&first_name=True&surname=True"
